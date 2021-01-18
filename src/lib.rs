73f898a4a1d6de75aa45ce045001f80fd411994a
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-18 23:22:49 +1100

    Add basic Event type

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub enum Event {
+    NewMessage { text: String },
 }
