07e9f3745e5fcd18a52049c6494b2f876fef1e2a
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-21 02:04:36 +0000

    Add User type and associate users with messages (#2)
    Closes #1. We implement rand’s traits here because we wouldn’t be able to implement them in babilado-client or babilado-server due to the [orphan rule](https://doc.rust-lang.org/book/ch10-02-traits.html#implementing-a-trait-on-a-type).

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,42 @@
+use rand::distributions::{Distribution, Standard};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
 pub enum Event {
-    NewMessage { text: String },
+    NewMessage(Message),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Message {
+    pub body: String,
+    pub author: UserId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct User {
+    pub nickname: Nickname,
+    pub tag: Tag,
+    pub id: UserId,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Nickname(pub String);
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct Tag(u16);
+
+impl Distribution<Tag> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Tag {
+        Tag(rng.gen_range(0..=9999))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct UserId(u64);
+
+impl Distribution<UserId> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> UserId {
+        UserId(rng.gen())
+    }
 }
